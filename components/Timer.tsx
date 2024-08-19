f8454e6fefd6f876743720748f5cf296986e08e1
Ditotisi Rasyid Sumpena <ditotisi.rasyid@praktis.co>
2024-08-19 18:36:27 +0700

    fix: bug

--- components/Timer.tsx
+++ components/Timer.tsx
@@ -11,7 +11,7 @@ interface timerProps {
 const Timer = ({ onClick, startTime = 10, active, side, isResetting, playerName }: timerProps) => {
   const [counter, setCounter] = useState<number>(0);
   useEffect(() => {
-    setCounter(startTime * 3600);
+    setCounter(startTime * 60);
   }, [startTime]);
 
   useEffect(() => {
@@ -25,7 +25,7 @@ const Timer = ({ onClick, startTime = 10, active, side, isResetting, playerName 
 
   useEffect(() => {
     if (isResetting) {
-      setCounter(startTime * 3600);
+      setCounter(startTime * 60);
     }
   }, [isResetting]);
   return (
@@ -39,9 +39,9 @@ const Timer = ({ onClick, startTime = 10, active, side, isResetting, playerName 
         }}
         className={`text-7xl text-center flex items-center justify-center w-full aspect-video cursor-pointer ${
           side === "white" ? "bg-white text-black border-black md:rotate-0 rotate-180" : "bg-black text-white border-black"
-        } border font-orbitron ${Boolean(counter <= 3600) ? (side === "white" ? "animate-textBlack" : "animate-textWhite") : ""}`}
+        } border font-orbitron ${Boolean(counter <= 60) ? (side === "white" ? "animate-textBlack" : "animate-textWhite") : ""}`}
       >
-        {convertTwoDigit(Math.floor(counter / 3600))}:{convertTwoDigit(Math.floor(counter % 60))}
+        {convertTwoDigit(Math.floor(counter / 60))}:{convertTwoDigit(Math.floor(counter % 60))}
       </div>
       <h2 className={`font-semibold text-2xl text-center ${side === "black" ? "" : "hidden md:block"}`}>
         {playerName ? playerName : <span className="capitalize">{side} Player</span>}
